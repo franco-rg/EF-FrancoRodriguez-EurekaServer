e3d3bd2e787a10a269a09c1b9040f73c52e7bd72
franco-rg <francordg0@gmail.com>
2022-08-18 23:47:34 -0500

    add

---
+++ src/test/java/com/franco/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapia.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
